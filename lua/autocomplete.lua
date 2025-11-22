f7287e500b9856b017107df21ab2e7bf01966d8d
temkuz <temkuz@yandex.ru>
2025-11-22 17:17:42 +0500

    update

--- lua/autocomplete.lua
+++ lua/autocomplete.lua
@@ -1,20 +1,22 @@
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-require('lspconfig').rust_analyzer.setup({
+local rust_analyzer_config = {
     capabilities = capabilities,
     on_attach = function(client, bufnr)
         if vim.lsp.inlay_hint then
             vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
         end
-        -- Дополнительные настройки on_attach, если нужны
     end,
-    settings={
+    settings = {
         ['rust-analyzer'] = {
             -- workspace = {symbol={search={kind='all_symbols'}}},
             check = {command='clippy'}
         }
     }
-})
+}
+
+vim.lsp.config("rust_analyzer", rust_analyzer_config)
+vim.lsp.enable("rust_analyzer")
 
 local cmp = require('cmp') 
 
